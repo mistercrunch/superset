24a826b66741ffa3d32064ac728ab909f2cb4ccd
Maxime Beauchemin <maximebeauchemin@gmail.com>
2017-11-05 23:00:31 -0800

    Set up OAuth (#36)
    Project superset-at-lyft is used and configured here:
    https://console.developers.google.com/apis/credentials/oauthclient/66693564572-aq60djhno9rd2nfo0nbt3aurkoqntdjm.apps.googleusercontent.com?project=superset-at-lyft
    
    Added hmiles@lyft.com as another owner of this Google project
    
    The flow isn't super great and we could alter it by overriding the login
    view to simply show a [login with google] button instead of what is
    shown now

--- superset_config.py
+++ superset_config.py
@@ -1,4 +1,7 @@
+"""Config file for Superset"""
 import os
+from flask_appbuilder.security.manager import AUTH_OAUTH
+from flask_appbuilder.security.sqla.manager import SecurityManager
 
 ROW_LIMIT = 5000
 SUPERSET_WORKERS = 4
@@ -24,3 +27,43 @@ CSRF_ENABLED = True
 # Set this API key to enable Mapbox visualizations
 MAPBOX_API_KEY = os.getenv("CREDENTIALS_MAPBOX_API_KEY")
 ENABLE_PROXY_FIX = True
+
+AUTH_TYPE = AUTH_OAUTH
+AUTH_USER_REGISTRATION = True
+AUTH_USER_REGISTRATION_ROLE = "Alpha"
+
+OAUTH_PROVIDERS = [{
+    'name':'google', 'icon':'fa-google', 'token_key':'access_token',
+    'remote_app': {
+        'consumer_key': os.getenv('CREDENTIALS_SUPERSET_OAUTH_CONSUMER_KEY'),
+        'consumer_secret': os.getenv("CREDENTIALS_SUPERSET_OAUTH_SECRET"),
+        'base_url':'https://www.googleapis.com/oauth2/v2/',
+        'request_token_params': {
+            'scope': 'email profile',
+        },
+        'request_token_url':None,
+        'access_token_url':'https://accounts.google.com/o/oauth2/token',
+        'authorize_url':'https://accounts.google.com/o/oauth2/auth'
+    }
+}]
+
+
+class LyftSecurityManager(SecurityManager):
+    """Lyft-specific FAB security manager"""
+
+    def get_oauth_user_info(self, provider, resp=None):
+        """Overriding this method to force a @lyft.com email"""
+        user = self.appbuilder.sm.oauth_remotes[provider].get('userinfo')
+        email = user.data.get('email', '')
+
+        assert provider == 'google'
+        assert email.endswith('lyft.com')
+
+        return {
+            'username': "google_" + user.data.get('id', ''),
+            'first_name': user.data.get('given_name', ''),
+            'last_name': user.data.get('family_name', ''),
+            'email': user.data.get('email', '')
+        }
+
+CUSTOM_SECURITY_MANAGER = LyftSecurityManager
