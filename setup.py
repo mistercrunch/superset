d531b61b2b01ea7481680fe32d06ad907a25f2e0
Maxime Beauchemin <maximebeauchemin@gmail.com>
2024-03-14 15:21:07 -0700

    well well

--- setup.py
+++ setup.py
@@ -26,6 +26,12 @@ PACKAGE_JSON = os.path.join(BASE_DIR, "superset-frontend", "package.json")
 with open(PACKAGE_JSON) as package_file:
     version_string = json.load(package_file)["version"]
 
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+with open("requirements/base.in", encoding="utf-8") as f:
+    reqs = [l for l in f.readlines() if not l.startswith("#")]
+
 
 def get_git_sha() -> str:
     try:
@@ -50,6 +56,7 @@ with open(VERSION_INFO_FILE, "w") as version_file:
 setup(
     name="apache-superset",
     description="A modern, enterprise-ready business intelligence web application",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     version=version_string,
     packages=find_packages(),
@@ -68,6 +75,88 @@ setup(
             "superset=superset.extensions.metadb:SupersetShillelaghAdapter"
         ],
     },
+    install_requires=reqs,
+    extras_require={
+        "athena": ["pyathena[pandas]>=2, <3"],
+        "aurora-data-api": ["preset-sqlalchemy-aurora-data-api>=0.2.8,<0.3"],
+        "bigquery": [
+            "pandas-gbq>=0.19.1",
+            "sqlalchemy-bigquery>=1.6.1",
+            "google-cloud-bigquery>=3.10.0",
+        ],
+        "clickhouse": ["clickhouse-connect>=0.5.14, <1.0"],
+        "cockroachdb": ["cockroachdb>=0.3.5, <0.4"],
+        "cors": ["flask-cors>=2.0.0"],
+        "crate": ["crate[sqlalchemy]>=0.26.0, <0.27"],
+        "databend": ["databend-sqlalchemy>=0.3.2, <1.0"],
+        "databricks": [
+            "databricks-sql-connector>=2.0.2, <3",
+            "sqlalchemy-databricks>=0.2.0",
+        ],
+        "db2": ["ibm-db-sa>0.3.8, <=0.4.0"],
+        "dremio": ["sqlalchemy-dremio>=1.1.5, <1.3"],
+        "drill": ["sqlalchemy-drill>=1.1.4, <2"],
+        "druid": ["pydruid>=0.6.5,<0.7"],
+        "duckdb": ["duckdb-engine>=0.9.5, <0.10"],
+        "dynamodb": ["pydynamodb>=0.4.2"],
+        "solr": ["sqlalchemy-solr >= 0.2.0"],
+        "elasticsearch": ["elasticsearch-dbapi>=0.2.9, <0.3.0"],
+        "exasol": ["sqlalchemy-exasol >= 2.4.0, <3.0"],
+        "excel": ["xlrd>=1.2.0, <1.3"],
+        "firebird": ["sqlalchemy-firebird>=0.7.0, <0.8"],
+        "firebolt": ["firebolt-sqlalchemy>=1.0.0, <2"],
+        "gevent": ["gevent>=23.9.1"],
+        "gsheets": ["shillelagh[gsheetsapi]>=1.2.10, <2"],
+        "hana": ["hdbcli==2.4.162", "sqlalchemy_hana==0.4.0"],
+        "hive": [
+            "pyhive[hive]>=0.6.5;python_version<'3.11'",
+            "pyhive[hive_pure_sasl]>=0.7.0",
+            "tableschema",
+            "thrift>=0.14.1, <1.0.0",
+        ],
+        "impala": ["impyla>0.16.2, <0.17"],
+        "kusto": ["sqlalchemy-kusto>=2.0.0, <3"],
+        "kylin": ["kylinpy>=2.8.1, <2.9"],
+        "mssql": ["pymssql>=2.2.8, <3"],
+        "mysql": ["mysqlclient>=2.1.0, <3"],
+        "ocient": [
+            "sqlalchemy-ocient>=1.0.0",
+            "pyocient>=1.0.15, <2",
+            "shapely",
+            "geojson",
+        ],
+        "oracle": ["cx-Oracle>8.0.0, <8.1"],
+        "pinot": ["pinotdb>=0.3.3, <0.4"],
+        "playwright": ["playwright>=1.37.0, <2"],
+        "postgres": ["psycopg2-binary==2.9.6"],
+        "presto": ["pyhive[presto]>=0.6.5"],
+        "trino": ["trino>=0.328.0"],
+        "prophet": ["prophet>=1.1.5, <2"],
+        "redshift": ["sqlalchemy-redshift>=0.8.1, <0.9"],
+        "rockset": ["rockset-sqlalchemy>=0.0.1, <1"],
+        "shillelagh": ["shillelagh[all]>=1.2.10, <2"],
+        "snowflake": ["snowflake-sqlalchemy>=1.2.4, <2"],
+        "spark": [
+            "pyhive[hive]>=0.6.5;python_version<'3.11'",
+            "pyhive[hive_pure_sasl]>=0.7",
+            "tableschema",
+            "thrift>=0.14.1, <1",
+        ],
+        "teradata": ["teradatasql>=16.20.0.23"],
+        "thumbnails": ["Pillow>=10.0.1, <11"],
+        "vertica": ["sqlalchemy-vertica-python>=0.5.9, < 0.6"],
+        "netezza": ["nzalchemy>=11.0.2"],
+        "starrocks": ["starrocks>=1.0.0"],
+        "doris": ["pydoris>=1.0.0, <2.0.0"],
+    },
+    python_requires="~=3.9",
+    author="Apache Software Foundation",
+    author_email="dev@superset.apache.org",
     url="https://superset.apache.org/",
     download_url="https://www.apache.org/dist/superset/" + version_string,
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
 )
